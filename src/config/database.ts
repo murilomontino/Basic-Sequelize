3bff72cf1c0042a776a6462c68a3fc8e8fc38079
Murilo Montino <murilomontinojr@hotmail.com>
2021-10-07 12:11:53 -0300

    remove src

--- src/config/database.ts
+++
@@ -1,9 +0,0 @@
-import { DataTypes, Sequelize } from 'sequelize'
-
-const database = new Sequelize('postgres://root:root@localhost:5432/root', {
-    dialect: 'postgres'
-}) // Example for postgres
-
-
-
-export default database
